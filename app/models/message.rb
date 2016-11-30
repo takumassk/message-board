7dd1d478707ee958374b7df50ef0c4bbca3d9d3c
takumasasaki <sasaki.oneofthem@gmail.com>
2016-11-30 16:21:43 +0000

    age validation add

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,6 @@ class Message < ActiveRecord::Base
     validates :name, length: {maximum: 20}, presence: true
     #内容は必須入力かつ二文字以上３０文字以内
     validates :body, length: {minimum: 2, maximum: 30 }, presence: true
-    #年齢は必須入力かつ二文字以上３０文字以内
-    validates :age, length: {minimum: 0, maximum: 150 }, numericality: true
+    #年齢は必須入力かつ二文字以上150文字以内
+    validates :age, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to:150 }, presence: true
 end
